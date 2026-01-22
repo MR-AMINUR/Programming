fa9fece32a684ada735436596c92d8b447b6dbc7
AMINUR <aminur01mallick@gmail.com>
2026-01-22 08:55:28 +0530

    String Duplication Detection.

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -104,6 +104,27 @@ class Structures {
 
         return totalWays;
     }
+
+    public void removeDuplicates(String str, int idx, StringBuilder newStr, boolean[] map) {
+
+        if (idx == str.length())
+        {
+            System.out.println(newStr);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+
+        if (map[currChar - 'a'] == true)
+        {
+            removeDuplicates(str, idx+1, newStr, map);
+        }
+        else
+        {
+            map[currChar-'a'] = true;
+            removeDuplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    }
 }
 
 
@@ -115,8 +136,8 @@ public class Codes {
         Structures sr = new Structures();
 
         
-        System.out.print(sr.tilingProblem(4));
-
+        String str = "appnnacollege";
+        sr.removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
         
         sc.close();
     }
