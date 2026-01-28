3ad09abcf0e580f7643db1597a28c954c7329c8a
AMINUR <aminur01mallick@gmail.com>
2026-01-28 11:48:09 +0530

    Merge Sort.

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -294,6 +294,57 @@ class Structures {
 
         return match;
     }
+
+    public void MergeSort(int[] arr, int si, int ei) {
+
+        if (ei <= si)
+        {
+            return;
+        }
+        int mid = si + (ei-si)/2;
+        MergeSort(arr, si, mid);
+        MergeSort(arr, mid+1, ei);
+        Merge(arr,si,mid,ei);
+    }
+
+    public void Merge(int[] arr, int si, int mid, int ei) {
+        int[] temp = new int[ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+
+        while (i <= mid && j <= ei)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid)
+        {
+            temp[k++] = arr[i++]; 
+        }
+        while (j <= ei)
+        {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i = si; k<temp.length; k++,i++)
+        {
+            arr[i] = temp[k];
+        }
+
+    }
+
+    
 }
 
 
@@ -304,12 +355,23 @@ public class Codes {
         Scanner sc = new Scanner(System.in);
         Structures sr = new Structures();
 
-        System.out.print("Enter the String: ");
-        String str = sc.next();
+        System.out.print("Enter the size of the Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the Array Elements: ");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
 
-        int n = str.length();
+        sr.MergeSort(arr, 0, n-1);
 
-        System.out.println(sr.MatchSubStrings(str, 0, n-1, n));
+        System.out.println("The Sorted Array: ");
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
 
         
         
