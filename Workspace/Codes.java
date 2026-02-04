a31825b2987d9f427e448ac35ea9e28a7958bd9c
AMINUR <aminur01mallick@gmail.com>
2026-02-04 09:00:01 +0530

    Inverse Count(Optimal Approach)

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -566,7 +566,67 @@ class Structures {
         return count;
     }
     
-    
+    public int InverseMerge(int[] arr, int si, int mid, int ei) {
+
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+        int invCount = 0;
+        int[] temp = new int[ei-si+1];
+
+        while (i <= mid && j <= ei)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+                invCount += mid-i+1;
+            }
+            k++;
+        }
+
+        while (i <= mid)
+        {
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei)
+        {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i= si; k < temp.length; k++, i++)
+        {
+            arr[i] = temp[k];
+        }
+
+        return invCount;
+    }
+
+    public int InverseMergeSort(int[] arr, int si, int ei) {
+
+        int invCount = 0;
+        if (si < ei)
+        {
+            int mid = (si+ei)/2;
+        
+            invCount = InverseMergeSort(arr, si, mid);
+            invCount += InverseMergeSort(arr, mid+1, ei);
+            invCount += InverseMerge(arr, si, mid, ei);
+        }
+
+        return invCount;
+    }
+
+    public int getInverseCount(int[] arr) {
+        int n = arr.length;
+
+        return InverseMergeSort(arr,0,n-1);
+    }
 }
 
 
@@ -587,7 +647,7 @@ public class Codes {
             arr[i] = sc.nextInt();
         }
 
-        System.out.print("The Inversions are: " + sr.InversionCount(arr));
+        System.out.print("The Inversions are: " + sr.getInverseCount(arr));
 
         
         
