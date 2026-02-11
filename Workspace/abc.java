82844373aba50598d26f274327ad959a29facfcf
AMINUR <aminur01mallick@gmail.com>
2026-02-11 22:56:12 +0530

    hi

---
+++ Workspace/abc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class abc {
+    public static void main(String[] args) {
+        
+        PrimeNumbers pl = new PrimeNumbers();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(pl.SwapSort("329643"));
+        
+        sc.close();
+    }
+}
+
---
+++ Workspace/badrul.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class badrul {
+    public static void main(String[] args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        
+
+       System.out.println("Hello Aminur");
+
+        
+        sc.close();
+    }
+}
