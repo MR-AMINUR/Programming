c30c1e00c1a92bea39b0becf1877ac485c239b80
AMINUR <aminur01mallick@gmail.com>
2026-02-16 19:09:39 +0530

    .

--- Workspace/Problem.java
+++ Workspace/Problem.java
@@ -1887,6 +1887,35 @@ Explanation 2:
             end--;
         }
     }
+
+    /*Given an array A of size N. Rearrange the given array so that A[i] becomes A[A[i]] with O(1) extra space.
+Lets say N = size of the array. Then, following holds true :
+
+    All elements in the array are in the range [0, N-1]
+    N * N does not overflow for a signed integer
+
+
+Constraints:
+
+1 <= N <= 5×104
+
+0 <= A[i] <= N - 1
+
+The elements of A are distinct 
+
+Input Format
+
+The argument A is an array of integers
+
+Example 1:
+
+Input : [1, 0]
+Return : [0, 1]
+
+Example 2:
+
+Input : [0, 2, 1, 3]
+Return : [0, 1, 2, 3] */
 }
 
 
@@ -1897,7 +1926,7 @@ public class Problem {
         PrimeNumbers pl = new PrimeNumbers();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(pl.SwapSort("329643"));
+        System.out.println(pl.solve("329643"));
         
         sc.close();
     }
