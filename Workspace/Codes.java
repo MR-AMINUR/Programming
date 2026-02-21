d0855743b761ac44fca1ffa7dfa5f9f33215bc4f
AMINUR <aminur01mallick@gmail.com>
2026-02-21 11:25:52 +0530

    Maximum stored water (Brute Force).

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -781,8 +781,24 @@ class Structures {
         list.set(idx1, list.get(idx2));
         list.set(idx2, temp);
 
-        
+    }
 
+    public int storedWater(ArrayList<Integer> height) {
+
+        int maxWater = 0;
+
+        for (int i = 0; i < height.size(); i++)
+        {
+            for (int j = i+1; j < height.size(); j++)
+            {
+                int ht = Math.min(height.get(i), height.get(j));
+                int wt = j-i;
+                int water = ht * wt;
+                maxWater = Math.max(maxWater, water);
+            }
+        }
+
+        return maxWater;
     }
 }
 
@@ -794,21 +810,20 @@ public class Codes {
         Scanner sc = new Scanner(System.in);
         Structures sr = new Structures();
 
-       System.out.print("Enter the 1st swapping index: ");
-       int idx1 = sc.nextInt();
-       System.out.print("Enter the 2nd swapping index: ");
-       int idx2 = sc.nextInt();
+       
 
        ArrayList<Integer> list = new ArrayList<>();
        list.add(1);
-       list.add(5);
        list.add(8);
        list.add(6);
        list.add(2);
+       list.add(5);
+       list.add(4);
+       list.add(8);
+       list.add(3);
+       list.add(7);
 
-       System.out.println(list);
-       sr.arrayList(list, idx1, idx2);
-       System.out.println(list);
+       System.out.println(sr.storedWater(list));
 
         
         sc.close();
