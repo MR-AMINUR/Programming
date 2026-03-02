d57a667e639621fe3ecf6e74cd0cc4904984f1a9
AMINUR <aminur01mallick@gmail.com>
2026-03-02 09:10:37 +0530

    Lectures: Linked List.

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -959,6 +959,18 @@ class Structures {
     }
 }
 
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 
 
 public class Codes {
