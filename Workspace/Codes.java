7fe914b05786112cae42167db1327724e64641ab
AMINUR <aminur01mallick@gmail.com>
2026-03-12 22:18:45 +0530

    Changes Saved.

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -1388,9 +1388,6 @@ class Structures {
             right = nextRight;
             left = nextLeft;
         }
-
-        
-
     }
 }
 
@@ -1398,11 +1395,13 @@ class Node {
 
     int data;
     Node next;
+    Node prev;
 
     public Node(int data)
     {
         this.data = data;
         this.next = null;
+        this.prev = null;
     }
 
 }
