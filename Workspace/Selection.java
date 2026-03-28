4368d2de6aa029bf67b6bce7e33baac96d2c3b7b
AMINUR <aminur01mallick@gmail.com>
2026-03-28 23:25:24 +0530

    Daily Practices.

--- Workspace/Selection.java
+++ Workspace/Selection.java
@@ -326,6 +326,23 @@ class ArrayMath {
                 i++;
         }
     }
+
+    public void ans(int[] a) {
+
+        int n = a.length;
+        int k = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] != 1)
+            {
+                int temp = a[i];
+                a[i] = a[k];
+                a[k] = temp;
+                k++;
+            }
+        }
+        
+    }  
 }
 
 class ArrayPartition {
