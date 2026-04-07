cf39f77ca8ec9e993f43a35e8554c7fe21e9070c
AMINUR <aminur01mallick@gmail.com>
2026-04-07 10:59:38 +0530

    Palindrome check of number

--- Workspace/Problem.java
+++ Workspace/Problem.java
@@ -2026,7 +2026,94 @@ Explanation 2:
         return result;
     }
 
-    
+    /*
+    Problem Description
+ 
+ 
+
+    Determine whether an integer is a palindrome. Do this without extra space.
+
+    A palindrome integer is an integer x for which reverse(x) = x where reverse(x) is x with its digit reversed. Negative numbers are not palindromic.
+
+
+    Problem Constraints
+    INT_MIN <= A <= INT_MAX
+
+
+    Input Format
+    The first argument is an integer A.
+
+
+    Output Format
+    Return 1 if A is a Palindrome Integer else return 0.
+
+
+    Example Input
+    Input 1:
+
+    A = 12121
+
+    Input 2:
+
+    A = 123
+
+
+
+    Example Output
+    Output 1:
+
+    1
+
+    Output 2:
+
+    0
+
+
+
+    Example Explanation
+    Explanation 1:
+
+    12121 when reversed will be 12121, and 12121 = 12121, hence a palindrome number.
+
+
+    Explanation 2:
+
+    123 when reversed will be 321, and 123 != 321, hence not a palindrome number
+    */
+
+    public int PalindromeCheck(int n) {
+
+        if (n < 0 || (n % 10 == 0 && n != 0))
+        {
+            return -1;
+        }
+
+        int reverse = 0;
+        while (n > reverse)
+        {
+            int digit = n % 10;
+            reverse = reverse*10 + digit;
+            n = n / 10;
+        }
+
+        if (reverse == n || (n == reverse/10))
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    // public boolean isPalindrome(int n) {
+
+    //     int rnum = DigitAccess(n);
+
+    //     if (rnum != n) 
+    //     {
+    //         return false;
+    //     }
+    //     return true;
+    // }
 }
 
 
@@ -2040,7 +2127,7 @@ public class Problem {
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
 
-        System.out.print(pl.DigitToString(n));
+        System.out.print(pl.PalindromeCheck(n));
         
         
         sc.close();
