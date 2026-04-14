1fe2b903d024b91179a660e551d2932c13606b3e
AMINUR <aminur01mallick@gmail.com>
2026-04-14 09:44:27 +0530

    Queue Implementations(basic op)

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -2007,6 +2007,61 @@ class Structures {
 
 }
 
+class Queue {
+
+    public int[] arr;
+    public int size;
+    public int rear;
+
+    public Queue(int n) {
+        arr = new int[n];
+        size = n;
+        rear = -1;
+    }
+
+    public boolean IsEmpty() {
+        return rear == -1;
+    }
+
+    public void add(int data) {
+        if (rear == size - 1)
+        {
+            System.out.println("Queue is full");
+            return;
+        }
+
+        rear += 1;
+        arr[rear] = data;
+    }
+
+    public int drop() {
+        if (IsEmpty())
+        {
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+
+        int front = arr[0];
+        for (int i = 0; i < rear; i++)
+        {
+            arr[i] = arr[i+1];
+        }
+        rear = rear - 1;
+        return front;
+    }
+
+    public int peep() {
+        if (IsEmpty())
+        {
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+
+        return arr[0];
+    }
+    
+}
+
 class Node {
 
     int data;
@@ -2028,13 +2083,18 @@ public class Codes {
     public static void main(String[] args) throws Exception{
 
         Scanner sc = new Scanner(System.in);
-        Structures sr = new Structures();
+        //Structures sr = new Structures();
         
-        System.out.print("Enter the String: ");
-        String str = sc.nextLine();
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
 
-        System.out.print(sr.SmallestnextPalindrome(str));
-        
+        while (!q.IsEmpty())
+        {
+            System.out.print(q.peep() + " ");
+            q.drop();
+        }
 
         sc.close();
     }
