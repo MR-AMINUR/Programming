8ff73d982b8195883ccb39c99faae848c1a9ef70
AMINUR <aminur01mallick@gmail.com>
2026-04-20 22:21:12 +0530

    some changes.......

--- Workspace/Selection.java
+++ Workspace/Selection.java
@@ -61,6 +61,18 @@ class MaxStack {
         return maxStack.peek();
     }
 
+    public int gcd(int a, int b) {
+
+        if (b == 0)
+        {
+            return a;
+        }
+        else 
+        {
+            return gcd(b, a % b);
+        }
+    }
+
     public boolean isEmpty() {
 
         return mainStack.isEmpty();
