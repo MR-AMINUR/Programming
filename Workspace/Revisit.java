f88b84b67df084f4bcde0be4d6c52201dc1387cf
AMINUR <aminur01mallick@gmail.com>
2026-07-04 23:24:36 +0530

    Add Queue implementation with basic operations to Revisit.java

--- Workspace/Revisit.java
+++ Workspace/Revisit.java
@@ -74,23 +74,73 @@ class Churning {
         reverseStack(s);
         PushAtBottom(s, top);
     }
+
+    
+}
+
+class Queue {
+    static int[] arr;
+    static int size;
+    static int rear;
+
+    Queue(int n) {
+        arr = new int[n];
+        size = n;
+        rear = -1;
+    }
+
+    public boolean isEmpty() {
+        return rear == -1;
+    }
+
+    public void add(int data) {
+        if (rear == size - 1) {
+            System.out.println("Queue is Housefull....");
+            return;
+        }
+
+        rear = rear + 1;
+        arr[rear] = data;
+    }
+
+    public int remove() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty....");
+            return -1;
+        }
+
+        int front = arr[0];
+        for (int i = 0; i < rear; i++)
+        {
+            arr[i] = arr[i+1];
+        }
+        rear = rear - 1;
+
+        return front;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty....");
+            return -1;
+        }
+
+        return arr[0];
+    }
 }
 
 public class Revisit {
     
     public static void main(String[] args) {
 
-        Churning ch = new Churning();
+        Queue q = new Queue(5);
+        q.add(2);
+        q.add(4);
+        q.add(6);
 
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-
-        ch.reverseStack(s);
-        while (!s.isEmpty())
-        {
-            System.out.println(s.pop());
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
         }
 
     }
