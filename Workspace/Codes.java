114bcca57329fe13c8689120a343fdd42846225e
AMINUR <aminur01mallick@gmail.com>
2026-07-06 09:01:05 +0530

    Greedy Algorithms.....

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -2017,7 +2017,33 @@ class Structures {
         }
     }
 
-    
+    /*-------------------------------Greedy Approaches--------------------------------------- */
+
+    public void ActivitySelection(int[] start, int[] end) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int maxAct = 0;
+        
+        maxAct = 1;
+        ans.add(0);
+        int lastEnd = end[0];
+
+        for (int i = 1; i < end.length; i++) 
+        {
+            if (start[i] >= lastEnd)
+            {
+                maxAct++;
+                ans.add(i);
+                lastEnd = end[i];
+            }
+        }
+
+        System.out.println("Maximum Activity: "+ maxAct);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
+    }
 
 }
 
@@ -2097,18 +2123,11 @@ public class Codes {
     public static void main(String[] args) throws Exception{
 
         Scanner sc = new Scanner(System.in);
-        //Structures sr = new Structures();
-        
-        Queue q = new Queue(5);
-        q.add(1);
-        q.add(2);
-        q.add(3);
+        Structures sr = new Structures();
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
 
-        while (!q.IsEmpty())
-        {
-            System.out.print(q.peep() + " ");
-            q.drop();
-        }
+        sr.ActivitySelection(start, end);
 
         sc.close();
     }
