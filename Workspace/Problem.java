7b080dd8f3cd22821e1b2bdda96bb261ef249838
AMINUR <aminur01mallick@gmail.com>
2026-07-06 07:53:10 +0530

    Unnecessary file deleted

--- Workspace/Problem.java
+++ Workspace/Problem.java
@@ -2377,64 +2377,83 @@ Explanation 2:
     }
 
     /*
-    Problem Description
+        Problem Description
  
  
 
-Given a large number represent in the form of an integer array A, where each element is a digit.
+        Given a large number represent in the form of an integer array A, where each element is a digit.
 
-You have to find whether there exists any permutation of the array A such that the number becomes divisible by 60.
+        You have to find whether there exists any permutation of the array A such that the number becomes divisible by 60.
 
-Return 1 if it exists, 0 otherwise.
-
-
-Problem Constraints
-1 <= |A| <= 105
-0 <= Ai <= 9
+        Return 1 if it exists, 0 otherwise.
 
 
-Input Format
-The first argument is an integer array A.
+        Problem Constraints
+        1 <= |A| <= 105
+        0 <= Ai <= 9
 
 
-Output Format
-Return a single integer '1' if there exists a permutation, '0' otherwise.
+        Input Format
+        The first argument is an integer array A.
 
 
-Example Input
-Input 1:
-
-A = [0, 6]
-
-Input 2:
-
-A = [2, 3]
+        Output Format
+        Return a single integer '1' if there exists a permutation, '0' otherwise.
 
 
+        Example Input
+        Input 1:
 
-Example Output
-Output 1:
+        A = [0, 6]
 
-1
+        Input 2:
 
-Output 2:
-
-0
+        A = [2, 3]
 
 
 
-Example Explanation
-Explanation 1:
+        Example Output
+        Output 1:
 
-We can rearrange the digits to form 60, which is divisible by 60.
+        1
 
-Explanation 2:
+        Output 2:
 
-There are only two possible permutations: [23, 32].
-Both of them are not divisible by 60.
+        0
+
+
+
+        Example Explanation
+        Explanation 1:
+
+        We can rearrange the digits to form 60, which is divisible by 60.
+
+        Explanation 2:
+
+        There are only two possible permutations: [23, 32].
+        Both of them are not divisible by 60.
 
 
     */
+    
+    public int divisible60(int[] arr) {
+
+        int number = 0;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            number = (number * 10) + i;
+        }
+
+        if (number % 60 == 1)
+        {
+            return 1;
+        }
+        else 
+        {
+            return 0;
+        }
+    }
 }
 
 
@@ -2445,12 +2464,8 @@ public class Problem {
         PrimeNumbers pl = new PrimeNumbers();
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter a: ");
-        int a = sc.nextInt();
-        
-
-        System.out.print(pl.Fibo(a));
-        
+        int[] arr = {0, 1, 2};
+        System.out.println(pl.divisible60(arr));
         
         
         sc.close();
