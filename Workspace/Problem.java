bce61a0700ee1ac21b737e354592ede71d84ef17
AMINUR <aminur01mallick@gmail.com>
2026-07-19 11:16:18 +0530

    Add problem description and constraints for maximum co-prime divisor in Problem class

--- Workspace/Problem.java
+++ Workspace/Problem.java
@@ -2849,6 +2849,35 @@ Explanation 2:
         
         return -1;
     }
+
+    /*
+    Problem Description
+ 
+ 
+        You are given two positive numbers A and B. You need to find the maximum valued integer X such that:
+
+        X divides A i.e. A % X = 0
+        X and B are co-prime i.e. gcd(X, B) = 1
+        For example,
+
+        A = 30
+        B = 12
+        We return
+        X = 5
+
+
+        Problem Constraints
+        1<=A,B<=1e9
+
+
+        Input Format
+        First argument is A.
+        Second argument is B.
+
+
+        Output Format
+        Find the maximum valued integer X which satisfies the above properties.
+    */
 }
 
 
