cab590bb68732299be776b29b078f74b6d7a4ebd
AMINUR <aminur01mallick@gmail.com>
2026-07-20 12:17:20 +0530

    Refactor FindX method in Problem class; add problem description and constraints for string rank calculation

--- Workspace/Problem.java
+++ Workspace/Problem.java
@@ -2881,20 +2881,90 @@ Explanation 2:
 
     public int FindX (int a, int b) {
 
-        if (a % 1 == 0 && gcd (b, 1) == 1)
-        {
-            return 1;
-        }
-        
-        for (int i = 2; i <= a; i++) {
-            int x = i;
-            if (a % x == 0 && gcd(x, b) == 1) {
-                return x;
+        while (true) {
+            int g = gcd(a, b);
+            if (g == 1)
+            {
+                return a;
             }
-        }
 
-        return -1;
+            a /= g;
+        }
     }
+
+    /*
+    Problem Description
+ 
+ 
+
+Given a string A, find the rank of the string amongst its permutations sorted lexicographically. Note that the characters might be repeated. If the characters are repeated, we need to look at the rank in unique permutations. Look at the example for more details.
+
+NOTE: 
+
+    The answer might not fit in an integer, so return your answer % 1000003 where 1000003 is a prime number.
+    String A can consist of both lowercase and uppercase letters. Characters with lesser ASCII values are considered smaller, i.e., 'a' > 'Z'.
+
+
+
+Problem Constraints
+
+1 <= len(A) <= 1000000 
+
+
+Input Format
+
+First argument is a string A.
+
+
+Output Format
+
+Return an integer denoting the rank.
+
+
+Example Input
+
+Input 1:
+
+ A = "aba"
+
+Input 2:
+
+ A = "bca"
+
+
+
+Example Output
+
+Output 1:
+
+ 2
+
+Output 2:
+
+ 4
+
+
+
+Example Explanation
+
+Explanation 1:
+
+ The order permutations with letters 'a', 'a', and 'b' :
+    aab
+    aba 
+    baa
+ So, the rank is 2.
+
+Explanation 2:
+
+ The order permutations with letters 'a', 'b', and 'c' :
+    abc
+    acb 
+    bac
+    bca
+    cab
+    cba
+ So, the rank is 4. */
 }
 
 
@@ -2904,7 +2974,7 @@ public class Problem {
         Scanner sc = new Scanner(System.in);
         PrimeNumbers pl = new PrimeNumbers();
        
-        int result = pl.FindX(30, 12);
+        int result = pl.FindX(36, 35);
         System.out.println(result);
         
         sc.close();
