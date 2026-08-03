f76f15c5f55fee9eab1e8363319deaa7b7538bc1
AMINUR <aminur01mallick@gmail.com>
2026-08-03 10:07:43 +0530

    Add methods for path finding and lowest common ancestor in Structures class

--- Workspace/Codes.java
+++ Workspace/Codes.java
@@ -3380,6 +3380,63 @@ class Structures {
         recursiveKthLevel(root.left, level+1, k);
         recursiveKthLevel(root.right, level+1, k);
     }
+
+    public boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size()-1);
+
+        return false;
+    }
+
+    public Node LowestCommonAncestor(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        Node lca = path1.get(i-1);
+
+        return lca;
+    }
+
+    public Node lca(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftx = lca(root.left, n1, n2);
+        Node rightx = lca(root.right, n1, n2);
+
+        if (leftx == null) {
+            return rightx;
+        }
+        if (rightx == null) {
+            return leftx;
+        }
+
+        return root;
+    }
 }
 
 class Info {
@@ -3435,7 +3492,7 @@ public class Codes {
         subRoot.left = new Node(4);
         subRoot.right = new Node(5);
 
-        ss.recursiveKthLevel(root, 1, 3);
+        System.out.println(ss.lca(root, 4, 7).data);
         
         
     }
