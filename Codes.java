af3d21e52ba14a0c4d0a1c1bccab5937ccf47747
AMINUR <aminur01mallick@gmail.com>
2026-08-09 10:37:48 +0530

    Add child nodes to the binary tree for testing purposes

--- Codes.java
+++ Codes.java
@@ -3590,10 +3590,10 @@ public class Codes {
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
-        // root.left.left = new Node(4);
-        // root.left.right = new Node(5);
-        // root.right.left = new Node(6);
-        // root.right.right = new Node(7);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
         Node tree = new Node(1);
         tree.left = new Node(2);
         tree.right = new Node(3);
