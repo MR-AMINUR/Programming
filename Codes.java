d729b58228ffd96293fcf19da9b15db964a174fd
AMINUR <aminur01mallick@gmail.com>
2026-09-01 09:15:04 +0530

    Add method to find the largest binary search tree in a given tree; introduce BSTInfo class for tree information

--- Codes.java
+++ Codes.java
@@ -3715,6 +3715,33 @@ class Structures {
         return createBST(list, 0, list.size()-1);
     }
 
+    public int maxBST = 0;
+
+    public BSTInfo largestBST(Node root) {
+        if (root == null) {
+            return new BSTInfo(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        BSTInfo leftInfo = largestBST(root.left);
+        BSTInfo rightInfo = largestBST(root.right);
+
+        int size = leftInfo.size + rightInfo.size + 1;
+
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+
+        if (root.data <= leftInfo.max || root.data >= rightInfo.min) {
+            return new BSTInfo(false, size, min, max);
+        }
+
+        if (leftInfo.isBST && rightInfo.isBST) {
+            maxBST = Math.max(maxBST, size);
+            return new BSTInfo(true, size, min, max);
+        }
+
+        return new BSTInfo(false, size, min, max);
+    }
+
 }
 
 class Info {
@@ -3752,24 +3779,38 @@ class Node {
 
 }
 
+class BSTInfo {
+    boolean isBST;
+    int size;
+    int min;
+    int max;
 
+    public BSTInfo(boolean isBST, int size, int min, int max) {
+        this.isBST = isBST;
+        this.size = size;
+        this.min = min;
+        this.max = max;
+    }
+}
 
 public class Codes {
     public static void main(String[] args) throws Exception{
         Structures ss = new Structures();
         
 
-        Node root = new Node(8);
-        root.left = new Node(6);
+        Node root = new Node(50);
+        root.left = new Node(30);
         root.left.left = new Node(5);
-        root.left.left.left = new Node(4);
+        root.left.right = new Node(20);
 
-        root.right = new Node(10);
-        root.right.right = new Node(11);
-        root.right.right.right = new Node(12);
+        root.right = new Node(60);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
 
-        root = ss.balanceBST(root);
-        ss.preOrder(root);
+        BSTInfo Info = ss.largestBST(root);
+        System.out.println(ss.maxBST);
         
     }
 }
