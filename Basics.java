e29f2e9b9aca6a6e27661d3e06d45c0dcd01eb8d
AMINUR <aminur01mallick@gmail.com>
2026-09-10 23:14:13 +0530

    Refactor JavaBasics class: implement array reversal, add arrays, and enhance second largest element logic; update main method for testing

--- Basics.java
+++ Basics.java
@@ -1,20 +1,85 @@
 import java.util.*;
 
 class JavaBasics {
-    public static class Student implements Comparable<Student> {
+    /*
+        //Reverse an array in-place.
+        Find the second largest element without sorting.
+        //Move all zeroes to the end of an array.
+        Find the missing number from 1...N.
+        //Solve the Two Sum problem.
+        Find the maximum subarray sum.
+    */
 
-        String name;
-        int rank;
+    public ArrayList<Integer> moveZeros(ArrayList<Integer> list) {
+        int k = 0; 
+        for (int i = 0; i < list.size()-1; i++) {
+            if (list.get(i) != 0) {
+                int temp = list.get(i);
+                list.set(i, list.get(k));
+                list.set(k, temp);
 
-        public Student(String name, int rank) {
-            this.name = name;
-            this.rank = rank;
+                k++;
+            }
         }
 
-        @Override 
-        public int compareTo(Student s2) {
-            return this.rank - s2.rank;
+        return list;
+    }
+
+    public ArrayList<Integer> addArrays(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        
+        int i = list1.size()-1;
+        int j = list2.size()-1;
+        int carry = 0, sum = 0;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            
+            int digitA = (i >= 0) ? list1.get(i) : 0;
+            int digitB = (j >= 0) ? list2.get(j) : 0;
+            sum = digitA + digitB + carry;
+
+            list.add(sum%10);
+            carry = sum/10;
+            i--;
+            j--;
         }
+        Collections.reverse(list);
+        return list;
+    }
+
+    public ArrayList<Integer> arrayReversal(ArrayList<Integer> list) {
+
+        int st = 0; 
+        int en = list.size()-1;
+
+        while (st < en) {
+            int temp = list.get(st);
+            list.set(st, list.get(en));
+            list.set(en, temp);
+
+            st++;
+            en--;
+        }
+
+        return list;
+    }
+
+    public int secondLargest(ArrayList<Integer> list) {
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+
+        for (int li : list) {
+            if (li > highest) {
+                secondHighest = highest;
+                highest = li;
+                
+            } else if (li > secondHighest && li != highest) {
+                secondHighest = li;
+            }
+        }
+
+        return secondHighest;
     }
 }
 
@@ -23,15 +88,15 @@ public class Basics {
     
     public static void main(String[] args) {
         
-        PriorityQueue<JavaBasics.Student> pq = new PriorityQueue<>();
-        pq.add(new JavaBasics.Student("A", 4));
-        pq.add(new JavaBasics.Student("B", 5));
-        pq.add(new JavaBasics.Student("C", 1));
-        pq.add(new JavaBasics.Student("D", 3));
+        JavaBasics jb = new JavaBasics();
 
-        while (!pq.isEmpty()) {
-            System.out.println(pq.peek().name + "->" + pq.peek().rank);
-            pq.remove();
-        }
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        list.add(9);
+        list.add(2);
+        list.add(3);
+        
+        System.out.println(jb.secondLargest(list));
+        
     }
 }
