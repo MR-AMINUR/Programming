d93a3637a512bead5fe383a5d5ff051b436f99d3
AMINUR <aminur01mallick@gmail.com>
2026-09-22 10:14:04 +0530

    Add square root computation method; update main for testing

--- Problem.java
+++ Problem.java
@@ -2786,6 +2786,65 @@ Explanation 2:
 
         return ans;
     }
+
+    /*
+        Problem Description
+        
+        Given an integer A.
+
+        Compute and return the square root of A.
+
+        If A is not a perfect square, return floor(sqrt(A)).
+
+        DO NOT USE SQRT FUNCTION FROM STANDARD LIBRARY.
+
+        NOTE: Do not use sort function from standard library. Users are expected to solve this in O(log(A)) time.
+
+        Problem Constraints
+        0 <= A <= INTMAX
+
+        Input Format
+
+        The first and only argument given is the integer A.
+
+
+        Output Format
+
+        Return floor(sqrt(A))
+
+
+        Example Input
+
+        Input 1:
+
+        11
+
+        Input 2:
+
+        9
+
+        Example Output
+
+        Output 1:
+
+        3
+
+        Output 2:
+
+        3
+
+        Example Explanation
+
+        Explanation:
+
+        When A = 11 , square root of A = 3.316. It is not a perfect square so we return the floor which is 3.
+        When A = 9 which is a perfect square of 3, so we return 3.
+    */
+
+    public int sqrt(int n) {
+
+        return (int)Math.floor(Math.sqrt(n));
+    }
 }
 
 
@@ -2796,15 +2855,7 @@ public class Problem {
         PrimeNumbers pl = new PrimeNumbers();
         
         
-        ArrayList<ArrayList<Integer>> A = new ArrayList<>(List.of(
-            new ArrayList<>(List.of(1, 3, 5)),
-            new ArrayList<>(List.of(2, 6, 9)),
-            new ArrayList<>(List.of(3, 6, 9))
-        ));
-
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-        
+        System.out.println(pl.sqrt(11));
         
         sc.close();
 
