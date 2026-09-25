d02f97a6d09af90314e0e0cca5a5f8957932b46a
AMINUR <aminur01mallick@gmail.com>
2026-09-25 15:18:52 +0530

    Add problem descriptions and implementations for red zone detection and modular exponentiation; update main for testing

--- Problem.java
+++ Problem.java
@@ -3146,6 +3146,131 @@ Explanation 2:
 
         return minTime;
     }
+
+    /*
+        Problem Description
+        
+        There are many people who believe that the earth is flat and NASA is a scam. They call themselves Flat Earthers. They were already worried that the 6 feet social distancing rule might push some people out of the earth.
+
+        Now, they decided to think about a new issue. Since the earth is flat ( or so they think), let's imagine it as an infinite 2D grid. They have the coordinates of certain points which are orange zones and have some COVID-19 cases reported. Now each day the orange zones become more fatal. After d days, all the locations within a euclidean distance of d of a particular orange zone can be affected by this zone.
+
+        They know N orange zones, each of whose coordinates are given by (A[i][0], A[i][1]). Now, a location is considered a red zone if it is affected by at least B orange zones. You need to find the first day at which the first red zone is reported.
+
+        Problem Constraints
+
+        2 <= B <= N <= 100
+
+        0 <= A[i][0], A[i][1] <= 109
+
+        Input Format
+
+        The first argument contains a 2D array A of size N, denoting the coordinates of the orange zones.
+
+        The second argument contains an integer B.
+
+        Output Format
+
+        Return the first day at which the first red zone is reported.
+
+
+        Example Input
+
+
+        Input 1:
+
+        A : 
+        [
+            [8, 5]
+            [0, 4]
+            [3, 6]
+        ]
+        B : 3
+
+        Input 2:
+
+        A : 
+        [
+            [2, 3]
+            [9, 4]
+            [10, 3]
+        ]
+        B : 2
+
+        Example Output
+
+        Output 1:
+
+        5
+
+        Output 2:
+
+        1
+
+        Example Explanation
+
+
+        Explanation 1:
+
+        One of the red zones will be (5, 4) and it is within a distance of 5 from all the orange zones.
+
+        Explanation 2:
+
+        One of the red zones will be (9, 3) and it is within a distance of 1 from 2 orange zones.
+
+    */
+
+    /*
+    Problem Description
+ 
+        
+        Implement pow(x, n) % d.
+        In other words, given x, n and d,
+        Find (xn % d)
+        Note that remainders on division cannot be negative. In other words, make sure the answer you return is non-negative integer.
+
+
+        Problem Constraints
+        -109 <= x <= 109
+        0 <= n <= 109
+        1 <= d <= 109
+
+
+        Example Input
+        Input 1:
+
+        x = 2
+        n = 3
+        d = 3
+
+        Input 2:
+
+        x = 5
+        n = 2
+        d = 6
+
+
+
+        Example Output
+        Output 1:
+
+        2
+
+        Output 2:
+
+        1
+
+
+
+        Example Explanation
+        Explanation 1:
+
+        23 % 3 = 8 % 3 = 2.
+
+        Explanation 2:
+
+        52 % 6 = 25 % 6 = 1.
+    
+    */
 }
 
 
@@ -3155,13 +3280,7 @@ public class Problem {
         Scanner sc = new Scanner(System.in);
         PrimeNumbers pl = new PrimeNumbers();
         
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(8);
-        list.add(11);
-        list.add(3);
-        
-        System.out.println(pl.paintBoard(10, 1, list));
+        System.out.println((int)(Math.pow(2,3))%3);
         
         sc.close();
 
